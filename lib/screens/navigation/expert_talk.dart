1ad62977388989497239a190a2c28688028a3da1
Raj_Kushwaha <rkofficial360917@gmail.com>
2022-10-07 10:09:54 +0530

    added Expert Talk and updated Home Page

--- lib/screens/navigation/expert_talk.dart
+++ lib/screens/navigation/expert_talk.dart
@@ -7,10 +7,209 @@ class ExpertTalk extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expert Talk'),
+        title: const Text('Expert Talk'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('Expert Talk'),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Get To Know Us',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "We're here to help",
+              style: TextStyle(color: Color(0xFF3A3636), fontSize: 20),
+            ),
+            const Text(
+              'Feel Free to ask Questions on',
+              style: TextStyle(color: Color(0xFF3A3636), fontSize: 20),
+            ),
+            const Text(
+              'Various topics.',
+              style: TextStyle(color: Color(0xFF3A3636), fontSize: 20),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Fertilizer Expert',
+              style: TextStyle(
+                  color: Color(0xFF3A3636),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.asset(
+                        'assets/images/rohan_sharma.png',
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Rohan Sharma',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      ),
+                      const Text('Msc in agriculture'),
+                      Row(
+                        children: [
+                          const Icon(Icons.phone),
+                          const Text('+919229436658'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.message),
+                          const Text('rohan@gmail.com'),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              'Pesticide Expert',
+              style: TextStyle(
+                  color: Color(0xFF3A3636),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.asset(
+                        'assets/images/murray_isman.png',
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Murray Isman',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      ),
+                      const Text('Msc in agriculture'),
+                      Row(
+                        children: [
+                          const Icon(Icons.phone),
+                          const Text('+919479436658'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.message),
+                          const Text('murray@gmail.com'),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              'Livestock Expert',
+              style: TextStyle(
+                  color: Color(0xFF3A3636),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.asset(
+                        'assets/images/neha_patel.png',
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Neha Patel',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      ),
+                      const Text('Bsc in agriculture'),
+                      Row(
+                        children: [
+                          const Icon(Icons.phone),
+                          const Text('+919229436658'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.message),
+                          const Text('neha@gmail.com'),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/profile/qualification.dart
+++ lib/screens/profile/qualification.dart
@@ -7,10 +7,18 @@ class Qualification extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Qualification'),
+        title: const Text('Qualification'),
       ),
-      body: Center(
-        child: Text('Qualification'),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: ListView.builder(
+            itemCount: 4,
+            itemBuilder: (ctx, i) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset('assets/images/cotton.jpg'),
+              );
+            }),
       ),
     );
   }
