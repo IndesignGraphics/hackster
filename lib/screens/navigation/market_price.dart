a88c0e32632487029af36f1619dd47dc8720697e
Raj_Kushwaha <rkofficial360917@gmail.com>
2022-10-09 14:07:26 +0530

    added market price screen

--- lib/screens/navigation/market_price.dart
+++ lib/screens/navigation/market_price.dart
@@ -1,17 +1,390 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MarketPrice extends StatelessWidget {
+class MarketPrice extends StatefulWidget {
   const MarketPrice({Key? key}) : super(key: key);
 
   @override
+  State<MarketPrice> createState() => _MarketPriceState();
+}
+
+class _MarketPriceState extends State<MarketPrice> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    String dropdownvalue = 'Rajkot';
+    
+    // List of items in our dropdown menu
+    var items = [
+      'Rajkot',
+      'Ahmedabad',
+      'Vadodara',
+      'Suraj',
+      'Gandhinagar',
+    ];
+
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
         appBar: AppBar(
           title: const Text('Market Price'),
         ),
-        body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Text('Market Price'),
-        ));
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        DropdownButton(
+                          dropdownColor: Theme.of(context).primaryColor,
+                          value: dropdownvalue,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownvalue = newValue!;
+                            });
+                          },
+                          items: items.map((String items) {
+                            return DropdownMenuItem(
+                              value: items,
+                              child: Text(
+                                items,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Vegetables',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Aug 09,2022',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SvgPicture.asset('assets/svgs/positive_graph.svg',
+                            height: 70),
+                        Text(
+                          '+1.50%',
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 50,
+                child: AppBar(
+                  backgroundColor: Colors.white,
+                  bottom: TabBar(
+                    indicatorWeight: 3,
+                    labelColor: Theme.of(context).primaryColor,
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    unselectedLabelStyle:
+                        TextStyle(fontWeight: FontWeight.normal),
+                    isScrollable: true,
+                    tabs: [
+                      Tab(
+                        text: 'Vegetables',
+                      ),
+                      Tab(
+                        text: 'Fruits',
+                      ),
+                      Tab(
+                        text: 'Cereals',
+                      ),
+                      Tab(
+                        text: 'Pulses',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (ctx, i) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 5),
+                          color: Colors.white,
+                          child: Center(
+                            child: Card(
+                              elevation: 1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset('assets/images/pea.png'),
+                                    const Text(
+                                      'Pea',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                        'assets/svgs/positive_graph.svg'),
+                                    Container(
+                                      height: 40,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: const [
+                                          Text(
+                                            '₹58.23',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            '+10.00%',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (ctx, i) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 5),
+                          color: Colors.white,
+                          child: Center(
+                            child: Card(
+                              elevation: 1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset('assets/images/pea.png'),
+                                    const Text(
+                                      'Apple',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                        'assets/svgs/negative_graph.svg'),
+                                    Container(
+                                      height: 40,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: const [
+                                          Text(
+                                            '₹58.23',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            '-10.00%',
+                                            style: TextStyle(
+                                                color: Colors.red,
+                                                fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    
+                    ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (ctx, i) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 5),
+                          color: Colors.white,
+                          child: Center(
+                            child: Card(
+                              elevation: 1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset('assets/images/pea.png'),
+                                    const Text(
+                                      'Wheat',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                        'assets/svgs/positive_graph.svg'),
+                                    Container(
+                                      height: 40,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: const [
+                                          Text(
+                                            '₹58.23',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            '+10.00%',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    
+                    ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (ctx, i) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 5),
+                          color: Colors.white,
+                          child: Center(
+                            child: Card(
+                              elevation: 1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset('assets/images/pea.png'),
+                                    const Text(
+                                      'Green Gram',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                        'assets/svgs/positive_graph.svg'),
+                                    Container(
+                                      height: 40,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: const [
+                                          Text(
+                                            '₹58.23',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            '+10.00%',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
