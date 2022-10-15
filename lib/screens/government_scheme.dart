da346a98cc6555df8c2a12dbc666145e42e8a4e7
RajKushwaha360 <rkofficial360917@gmail.com>
2022-10-15 19:05:36 +0530

    government_scheme and insurance added

--- lib/screens/government_scheme.dart
+++ lib/screens/government_scheme.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GovernmentScheme extends StatelessWidget {
   const GovernmentScheme({Key? key}) : super(key: key);
 
+  void _launchURL(String url) async {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+   static const _listOfObjects = [
+    {
+      'title': 'કિસાન પરીવાહન યોજના',
+      'description':
+          'કૃષિ ઉત્પાદનના પરિવહન માટે રાજ્યના ખેડૂતો સામાન્ય રીતે ટ્રેક્ટરનો ઉપયોગ કરતાં હોય છે તેમજ ઓછી જણસ હોય તે સંજોગોમાં ભાડુતી અન્ય ગુડ્ઝ કેરેઝ વાહન દ્વારા કૃષિ ઉત્પાદન ખેત બજારો કે અન્ય બજારમાં પહોંચાડતા હોય છે. ઘણી વખત પરિવહન માટે વાહન ઉપલબ્ધ ન હોય ત્યરે ખેડૂતોને મુશ્કેલીનો સામનો કરવો પડે છે. આમ, કૃષિ ઉત્પાદન પરિવહન સરળ બનાવવાના અને રાજ્યના ખેડૂતો ગુડ્ઝ કેરેજ વાહન વસાવી શકે તે હેતુથી સને ૨૦૨૦-૨૧થી રાજ્યના ખેડૂતોને મીડીયમ સાઇઝના ગુડ્ઝ કેરેઝ વાહનની ખરીદી ઉપર નાણાંકીય સહાય આપવાની યોજના શરૂ કરેલ છે. ૧૦૦% રાજ્ય પુરસ્કૃત યોજના છે.',
+      'link': 'https://agri.gujarat.gov.in/kisan-parivahan-yojna.htm'
+    },
+    {
+      'title': 'મુખ્ય મંત્રી પાક સંગ્રાહ માળખું યોજના',
+      'description':
+          'રાજ્યના દરેક વર્ગના ખેડૂતોને કૃષિ પેદાશોના સારા ઉત્પાદન મળવા છતાં કમોસમી વરસાદ, વાવાઝોડું, અતિવૃષ્ટી તેમજ અન્ય પરિબળોથી ઉત્પાદનની ગુણવત્તા પર માઠી અસર થાય છે તેમજ લાંબા સમય સુધી પાક સંગ્રહ માટેની જોઇ યોગ્ય વ્યવસ્થા ન હોવાના લીધે પાકનો બગાડ થાય છે. પાક ઉત્પાદન બચાવવા માટે રાજ્યના ખેડૂતોને ખેત પેદાશોના સંગ્રહની સગવડ મળી રહે તો પાક ઉત્પાદનને લાંબા સમય સુધી સુરક્ષિત રાખી શકાય છે અને ખેડૂતોને સુરક્ષિત પાક સંગ્રહ ગોડાઉન ઉપલબ્ધ થવાથી ખેત પેદાશોની ગુણવત્તા જળવાઇ રહે અને યોગ્ય સમયે વેચાણ કરી શકશે, જેથી ખેડૂતોને આર્થિક ફાયદો થાય અને જેના પરિણામે ખેતી વધુ નકાકારક બની શકે છે. તે હેતુથી રાજ્યના ખેડોતોને પાક સંગ્રહ સ્ટ્રક્ચર બનાવવા માટે સહાય આપવાની યોજના સને ૨૦૨૦-૨૧થી શરૂ કરવામાં આવેલ છે.૧૦૦% રાજ્ય પુરસ્કૃત યોજના છે.',
+      'link': 'https://agri.gujarat.gov.in/CM-Pak-sangrah-structure-yojna.htm'
+    },
+    {
+      'title': 'પીએમ કિસાન',
+      'description':
+          'ભારત સરકારશ્રી દ્વારા ખેડૂતોની આવક વધારવાના ભાગરૂપે ખેતી કરતા ખેડૂત કુટુંબને સહાયરૂપ થવા માટે ૧૦૦% કેન્દ્ર સહાયિત યોજના તરીકે પ્રધાનમંત્રી કિસાન સન્માન નિધિ યોજના જાહેર કરવામાં આવેલ છે.',
+      'link': 'https://agri.gujarat.gov.in/pm-kisan.htm'
+    },
+    {
+      'title': 'AGR-2 યોજના',
+      'description':
+          'રાજ્યમાં ઔદ્યોગિકીકરણ અને શહેરીકરણને કારણે ખેતમજૂરોની ઘણી અછત છે. ઓછા ખર્ચે અને વધુ ખેત ઉત્પાદન સાથે કૃષિ ઉદ્યોગોને સફળ બનાવવા માટે કૃષિ યાંત્રિકરણનો વિસ્તાર કરવો અનિવાર્ય છે. કૃષિ યાંત્રિકરણને કારણે જમીન, પાણી અને વાતાવરણ જેવા કુદરતી, બિનઅસરકારક સ્ત્રોતોનો મહત્તમ ઉપયોગ કરીને અને ઓછા ખર્ચે ખેડૂત સારી ગુણવત્તા અને વધુ ખેત ઉત્પાદન મેળવી શકે છે. આમ, કૃષિ યાંત્રીકરણ વધારવા માટે, AGR 2 (કૃષિ મશીનરી) યોજના હેઠળ સહાય આપવામાં આવે છે, રાજ્યના 208 તાલુકાઓમાં જ્યાં કેન્દ્ર દ્વારા પ્રાયોજિત "કૃષિ યાંત્રીકરણ પર સબમિશન" યોજના અમલમાં નથી.',
+      'link': 'https://agri.gujarat.gov.in/agr2-scheme.htm'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Government Scheme'),
+    return SingleChildScrollView(
+      child: Container(
+    height: MediaQuery.of(context).size.height - 140,
+    padding: EdgeInsets.all(10),
+    child: ListView.separated(
+        itemBuilder: (ctx, i) {
+          return Card(
+            elevation: 3,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    _listOfObjects[i]['title']!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    _listOfObjects[i]['description']!,
+                    style: TextStyle(),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      _launchURL(
+                        _listOfObjects[i]['link']!,
+                      );
+                    },
+                    child: Text(
+                      'View More',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (ctx, i) {
+          return SizedBox(
+            height: 10,
+          );
+        },
+        itemCount: _listOfObjects.length),
+      ),
     );
   }
 }
--- lib/widgets/credit_insurance/insurance.dart
+++ lib/widgets/credit_insurance/insurance.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Insurance extends StatelessWidget {
   const Insurance({Key? key}) : super(key: key);
 
+  void _launchURL(String url) async {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+   static const _listOfObjects = [
+    {
+      'title': 'ખાતેદાર ખેડૂત અસ્ક્માત વીમા યોજના',
+      'description':
+          'ગુજરાત સરકારે રાજ્યના ખાતેદાર ખેડૂતોને આકસ્મીક મૃત્યુ /કાયમી અંપગતાના કિસ્સામાં વીમા રક્ષણ આપવાની યોજના ૨૬ મી જાન્યુઆરી, ૧૯૯૬ ના શુભ દિવસથી આરંભ કરેલ છે. આ યોજના ૧૦૦% રાજ્ય સરકાર પુરુસ્કૃત યોજના છે. આ યોજનામાં રાજ્યના તમામ ખાતેદાર ખેડૂતો વતી રાજ્ય સરકારશ્રી દ્વારા વીમા પ્રિમીયમની ચુકવણી કરવામાં આવે છે. આ યોજના ગુજરાત સામુહિક જૂથ જનતા અકસ્માત વીમા યોજના હેઠળ તા.૦૧/૦૪/૨૦૦૮ થી વિમા નિયામકશ્રી, ગાંધીનગર મારફત અમલમાં છે.',
+      'link': 'https://agri.gujarat.gov.in/farmers-accidental-insurance-scheme.htm'
+    },
+    {
+      'title': 'પ્રધાન મંત્રી પાક વીમા યોજના',
+      'description':
+          'રાજયમાં પ્રઘાન મંત્રી ફસલ બીમા યોજના વર્ષ ૨૦૧૬-૧૭ ની ખરીફ,૨૦૧૬ ઋતુથી ભારત સરકારશ્રીની ગાઇડલાઇન મુજબ અમલી કરવામાં આવેલ છે.પ્રઘાન મંત્રી ફસલ બીમા યોજનામાં નીચે મુજબના ખરીફ ઋતુના કુલ ૧૬ અને રવિ/ઉનાળુ ઋતુના કુલ ૧૨ પાકો મળી કુલ ૨૮ પાકોને આવરી લેવામાં આવેલ છે.',
+      'link': 'https://pmfby.gov.in'
+    },
+    {
+      'title': 'બાયો-ફ્યુઅલ ટ્રી પ્લાન્ટ વીમો',
+      'description':
+          'વિશ્વની લગભગ 80% ઊર્જા જરૂરિયાતો અશ્મિભૂત ઇંધણ દ્વારા પૂરી થાય છે જે માંગમાં વધારાને કારણે દિવસેને દિવસે ઘટી રહી છે. આનાથી અમને વૈકલ્પિક ઉર્જા સ્ત્રોતોની શોધ કરવામાં આવી છે, જેમાં સૌથી વધુ આશાસ્પદ બાયો-ઇંધણ છે. આ પર્યાવરણને અનુકૂળ ઈંધણને પ્રોત્સાહન આપવા માટે, સરકાર બાયો-ઈંધણના વૃક્ષ/છોડ ઉગાડનારાઓને ઊર્જા સુરક્ષા પ્રાપ્ત કરવા માટે વિવિધ પ્રોત્સાહનો, વિમો અને સબસિડી આપી રહી છે.',
+      'link': 'https://agritech.tnau.ac.in/crop_insurance/crop_insurance_scheme_biofuel.html'
+    },
+    {
+      'title': 'રાષ્ટ્રીય કૃષિ વીમા યોજના',
+      'description':
+          '1. કુદરતી આફતો, જીવાતોના પરિણામે કોઈપણ સૂચિત પાક નિષ્ફળ જવાની સ્થિતિમાં ખેડૂતોને વીમા કવરેજ અને નાણાકીય સહાય પૂરી પાડવી\n2. કૃષિમાં પ્રગતિશીલ ખેતી પદ્ધતિઓ, ઉચ્ચ મૂલ્યના ઇનપુટ્સ અને ઉચ્ચ ટેકનોલોજી અપનાવવા ખેડૂતોને પ્રોત્સાહિત કરવા.\n3. ખેતીની આવકને સ્થિર કરવામાં મદદ કરવા માટે, ખાસ કરીને આપત્તિના વર્ષોમાં',
+      'link': 'https://www.aicofindia.com/AICGujarati/Pages/National-Agriculture-Insurance-Scheme-(NAIS).aspx'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Insurance'
+    return SingleChildScrollView(
+      child: Container(
+    height: MediaQuery.of(context).size.height - 130,
+    padding: EdgeInsets.all(10),
+    child: ListView.separated(
+        itemBuilder: (ctx, i) {
+          return Card(
+            elevation: 3,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    _listOfObjects[i]['title']!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    _listOfObjects[i]['description']!,
+                    style: TextStyle(),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      _launchURL(
+                        _listOfObjects[i]['link']!,
+                      );
+                    },
+                    child: Text(
+                      'View More',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (ctx, i) {
+          return SizedBox(
+            height: 10,
+          );
+        },
+        itemCount: _listOfObjects.length),
       ),
     );
   }
