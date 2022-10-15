0f01992dc5a30e9bda1489393b38cf36006a250c
IndesignGraphics <66730400+IndesignGraphics@users.noreply.github.com>
2022-10-15 15:02:28 +0530

    language file made

--- lib/screens/navigation/live_stock.dart
+++ lib/screens/navigation/live_stock.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class LiveStock extends StatelessWidget {
   const LiveStock({Key? key}) : super(key: key);
