4657dfa09fdcf3b2809f88c275f1e82adcfa9875
RajKushwaha360 <rkofficial360917@gmail.com>
2022-10-15 23:18:26 +0530

    tools_page added

--- lib/screens/tools_page.dart
+++ lib/screens/tools_page.dart
@@ -4,6 +4,105 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class ToolsPage extends StatelessWidget {
   const ToolsPage({Key? key}) : super(key: key);
 
+  static const _listOfFertilizers = [
+    {
+      'title': 'Chloride',
+      'image': 'assets/images/chloride.png',
+      'quantity': '1kg',
+      'price': '130',
+    },
+    {
+      'title': 'Loh tatva',
+      'image': 'assets/images/loh_iron.png',
+      'quantity': '1kg',
+      'price': '955',
+    },
+    {
+      'title': 'Sulphur',
+      'image': 'assets/images/sulphur.png',
+      'quantity': '5kg',
+      'price': '385',
+    },
+    {
+      'title': 'Calcium',
+      'image': 'assets/images/calcium.png',
+      'quantity': '5kg',
+      'price': '999',
+    },
+    {
+      'title': 'Boron',
+      'image': 'assets/images/boron.png',
+      'quantity': '1kg',
+      'price': '5000',
+    }
+  ];
+
+  static const _listOfPesticides = [
+    {
+      'title': 'Confidor Imidacloprid ',
+      'image': 'assets/images/imidacloprid.png',
+      'quantity': '100ml',
+      'price': '370',
+    },
+    {
+      'title': 'Dhanuka Aatank',
+      'image': 'assets/images/aatank.png',
+      'quantity': '1 ltr',
+      'price': '1180',
+    },
+    {
+      'title': 'Dhanuka Omite',
+      'image': 'assets/images/omite.png',
+      'quantity': '1 ltr',
+      'price': '1680',
+    },
+    {
+      'title': 'Utkarsh beveroz',
+      'image': 'assets/images/beveroz.png',
+      'quantity': '1 ltr',
+      'price': '608',
+    },
+    {
+      'title': 'Monosol',
+      'image': 'assets/images/monosol.png',
+      'quantity': '2 ltr',
+      'price': '1181',
+    }
+  ];
+
+  static const _listOfSeeds = [
+    {
+      'title': 'Brocolli Seeds',
+      'image': 'assets/images/brocolli_seeds.png',
+      'quantity': '1 pack',
+      'price': '105',
+    },
+    {
+      'title': 'Parsley Seeds',
+      'image': 'assets/images/parsley_seeds.png',
+      'quantity': '1 pack',
+      'price': '105',
+    },
+    {
+      'title': 'Thyme Seeds',
+      'image': 'assets/images/thyme_seeds.png',
+      'quantity': '1 pack',
+      'price': '105',
+    },
+    {
+      'title': 'Red Capsicum Seeds',
+      'image': 'assets/images/red_capsicum_seeds.png',
+      'quantity': '1 pack',
+      'price': '105',
+    },
+    {
+      'title': 'Desi Carrot Seeds',
+      'image': 'assets/images/desi_carrot_seeds.png',
+      'quantity': '1 pack',
+      'price': '87',
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -29,7 +128,7 @@ class ToolsPage extends StatelessWidget {
                 ),
                 Expanded(
                   child: Text(
-                    AppLocalizations.of(context)?.fertilizers??'Fertilizers',
+                    AppLocalizations.of(context)?.fertilizers ?? 'Fertilizers',
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 18,
@@ -37,7 +136,7 @@ class ToolsPage extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  AppLocalizations.of(context)?.viewAll??'View All',
+                  AppLocalizations.of(context)?.viewAll ?? 'View All',
                   style: const TextStyle(
                       color: Colors.blue, fontWeight: FontWeight.bold),
                 ),
@@ -53,7 +152,7 @@ class ToolsPage extends StatelessWidget {
               height: 230,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemCount: 5,
+                itemCount: _listOfFertilizers.length,
                 itemBuilder: (ctx, i) {
                   return Card(
                     elevation: 3,
@@ -73,21 +172,16 @@ class ToolsPage extends StatelessWidget {
                         children: [
                           ClipRRect(
                             borderRadius: BorderRadius.circular(10),
-                            child: Image.asset('assets/images/cotton.jpg'),
+                            child: Image.asset(
+                              _listOfFertilizers[i]['image']!,
+                              height: 130,
+                            ),
                           ),
                           const SizedBox(
                             height: 10,
                           ),
-                          const Text(
-                            'UREA',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const Text(
-                            'by Southern Petrochemicals',
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
+                          Text(
+                            _listOfFertilizers[i]['title']!,
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                             ),
@@ -95,15 +189,15 @@ class ToolsPage extends StatelessWidget {
                           const SizedBox(
                             height: 5,
                           ),
-                          const Text(
-                            'From ₹266.00',
+                          Text(
+                            'From ₹${_listOfFertilizers[i]['price']}',
                             style: TextStyle(
                               color: Colors.teal,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          const Text(
-                            '45kg',
+                           Text(
+                            _listOfFertilizers[i]['quantity']!,
                             style: TextStyle(
                               color: Colors.grey,
                               fontWeight: FontWeight.bold,
@@ -134,7 +228,107 @@ class ToolsPage extends StatelessWidget {
                 ),
                 Expanded(
                   child: Text(
-                    AppLocalizations.of(context)?.pesticides??'Pesticides',
+                    AppLocalizations.of(context)?.pesticides ?? 'Pesticides',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Text(
+                  AppLocalizations.of(context)?.viewAll ?? 'View All',
+                  style: const TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 230,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _listOfPesticides.length,
+                itemBuilder: (ctx, i) {
+                  return Card(
+                    elevation: 3,
+                    clipBehavior: Clip.hardEdge,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      width: 170,
+                      height: 220,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              _listOfPesticides[i]['image']!,
+                              height: 130,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                           Text(
+                            _listOfPesticides[i]['title']!,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                           Text(
+                            'From ₹${_listOfPesticides[i]['price']!}',
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                           Text(
+                            _listOfPesticides[i]['quantity']!,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.blueGrey.shade100,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Icon(Icons.compost),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Text(
+                    AppLocalizations.of(context)?.herbicides ?? 'Seeds',
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 18,
@@ -178,21 +372,13 @@ class ToolsPage extends StatelessWidget {
                         children: [
                           ClipRRect(
                             borderRadius: BorderRadius.circular(10),
-                            child: Image.asset('assets/images/cotton.jpg'),
+                            child: Image.asset(_listOfSeeds[i]['image']!,height: 130,),
                           ),
                           const SizedBox(
                             height: 10,
                           ),
-                          const Text(
-                            'Regent SC',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const Text(
-                            'by Bayer',
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
+                           Text(
+                            _listOfSeeds[i]['title']!,
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                             ),
@@ -200,120 +386,15 @@ class ToolsPage extends StatelessWidget {
                           const SizedBox(
                             height: 5,
                           ),
-                          const Text(
-                            'From ₹1350.00',
+                           Text(
+                            'From ₹${_listOfSeeds[i]['price']}',
                             style: TextStyle(
                               color: Colors.teal,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          const Text(
-                            '3 SKU available',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey.shade100,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Icon(Icons.compost),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: Text(
-                    AppLocalizations.of(context)?.herbicides ?? 'Herbicides',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                Text(
-                  AppLocalizations.of(context)?.viewAll??'View All',
-                  style: const TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 230,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (ctx, i) {
-                  return Card(
-                    elevation: 3,
-                    clipBehavior: Clip.hardEdge,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Container(
-                      width: 170,
-                      height: 220,
-                      padding: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.asset('assets/images/cotton.jpg'),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text(
-                            'Ricestar',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const Text(
-                            'by BASF',
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Text(
-                            'From ₹1300.00',
-                            style: TextStyle(
-                              color: Colors.teal,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const Text(
-                            '500ml',
+                           Text(
+                            _listOfSeeds[i]['quantity']!,
                             style: TextStyle(
                               color: Colors.grey,
                               fontWeight: FontWeight.bold,
